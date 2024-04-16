74e17f8adb93f2af5f2a310d343d75722088e977
Politeg <104119904+Politeg@users.noreply.github.com>
2024-04-16 17:56:15 +0500

    Delete partners directory

--- partners/schedule_events/index.php
+++
@@ -1,4 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Расписание мероприятий");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
